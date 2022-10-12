a85ba6f673a87805e043458dd493888a0913ab57
Andre Dragacevac <a.dragacevac@icloud.com>
2022-10-12 16:28:56 -0400

    commit tree 4bc59d692258969950c8872556fe7d6b6e5f448e

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let greeting = 'This one is on me!';
+  if (feet<=400){
+  return greeting;
+  }
+  else if (feet>400 && feet<=2000){
+  greeting = 'That will be twenty bucks.'
+  return greeting;
+  }
+  if (feet>2000 && feet<=2500){
+  greeting = 'I will gladly take your thirty bucks.'
+  return greeting;
+  }
+  else 
+  greeting = 'No can do.'
+  return greeting;
+}
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+    return "Ok, sounds good."
+  }
+  else 
+  return "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+if (tip === "generous"){
+  return "Thank you so much."
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+else if (tip === "not as generous"){
+  return "Thank you."
+}
+else
+return "Bye."
 }
